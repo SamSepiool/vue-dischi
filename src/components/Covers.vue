3674c33a14692797e7ccc51eb33af95753121bff
SamSepiool <simone.severini@outlook.it>
2021-10-05 01:03:31 +0200

    add filter

--- src/components/Covers.vue
+++ src/components/Covers.vue
@@ -1,87 +1,76 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@SamSepiool 
-SamSepiool
-/
-vue-dischi
-Public
-1
-00
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-vue-dischi/src/components/Covers.vue
-@SamSepiool
-SamSepiool fix Canvas props
-Latest commit 85118e7 yesterday
- History
- 1 contributor
-46 lines (35 sloc)  745 Bytes
-   
 <template>
- 
-    <div class="covers_box">
-        <Canvas :api="album" v-for="(album, index) in covers" :key="index"/>
-    </div>
-     
+  <div class="covers_box">
+    <Canvas
+      :api="album"
+      v-for="(album, index) in filteredBy"
+      :key="index"
+    />
+  </div>
 </template>
 
 <script>
 import Canvas from "./Canvas.vue";
 import axios from "axios";
 export default {
-    name: 'Covers',
-    components:{
-        Canvas
-    },
-    data(){
-        return{
-            covers: [],
-            genres: [],
-            authors: []
-        }
-    },
-       created() {
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then( (response) => {
-            this.covers = response.data.response;
+  name: "Covers",
+  props:{
+      genre: String
+  },
+  components: {
+    Canvas,
+  },
+  data() {
+    return {
+      covers: [],
+      genres: [],
+      authors: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        this.covers = response.data.response;
 
-            this.covers.forEach ((elm) => {
-                if ( !this.genres.includes(elm.genre) ) {
-                        this.genres.push(elm.genre);
-                    } 
-                if ( !this.authors.includes(elm.author) ) {
-                    this.authors.push(elm.author)
-                }
-                // (!this.genres.includes(elm.genre)) ?  this.genres.push(elm.genre) : null
-                // (!this.authors.includes(elm.author)) ? this.authors.push(elm.author) : null 
-            });
-            console.log(this.authors)
-            console.log(this.genres)
+        this.covers.forEach((elm) => {
+          if (!this.genres.includes(elm.genre)) {
+            this.genres.push(elm.genre);
+          }
+          if (!this.authors.includes(elm.author)) {
+            this.authors.push(elm.author);
+          }
+          // (!this.genres.includes(elm.genre)) ?  this.genres.push(elm.genre) : null
+          // (!this.authors.includes(elm.author)) ? this.authors.push(elm.author) : null
         });
+        console.log(this.authors);
+        console.log(this.genres);
+      });
+  },
+
+  computed: {
+    filteredBy() {
+      const coversFiltered = this.covers.filter((elm) => {
+        if (this.genre != "") {
+          return elm.genre.toLowerCase().includes(this.genre.toLowerCase());
+        }
+        if (this.genre == "") {
+          return this.covers;
+        }
+      });
+      return coversFiltered;
     },
-}
+  },
+};
 </script>
 
 
 
 <style lang='scss'>
 @import "../assets/style/variables.scss";
-.covers_box{
-    max-width: 71.875rem;
-    margin: auto;
-    display: flex;
-    flex-wrap: wrap;
+.covers_box {
+  max-width: 71.875rem;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
