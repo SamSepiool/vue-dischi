b7ca8eaf090de06e5dbf4815926643a84a26258b
SamSepiool <simone.severini@outlook.it>
2021-10-04 23:33:31 +0200

    add Nav component + $emit genre

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,31 @@
        <div class="logo">
            <img src="../assets/img/spotify-logo.png" alt="">
        </div>
+
+        <Nav @changedGenre='selectedGenre'/>
+
    </header>
 </template>
 
 <script>
+import Nav from "./Nav.vue"
+
 export default {
     name: 'Header',
+    components: {
+        Nav
+    },
+    data(){
+        return{
+            choosenGenre: ''
+        }
+    },
+    methods:{
+
+        selectedGenre(selectValue){
+            this.choosenGenre = selectValue;
+        }
+    }
     
 }
 </script>
@@ -20,6 +39,9 @@ header{
     max-height: 4.375rem;
     background-color: $header;
     padding: .625rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     .logo{
         max-width: 3.75rem;
---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <select name="genres" v-model="genreSelected" @change="$emit('changedGenre', genreSelected)">
+            <option value=""></option>
+            <option value="rock">ROCK</option>
+            <option value="pop">POP</option>
+            <option value="metal">METAL</option>
+            <option value="jazz">JAZZ</option>
+        </select>  
+    </nav>
+  
+</template>
+
+<script>
+export default {
+    name: "Nav",
+    emits: '',
+    data(){
+        return{
+            genreSelected: ''
+        }
+    },
+}
+
+</script>
+
+<style lang="scss">
+
+
+</style>
